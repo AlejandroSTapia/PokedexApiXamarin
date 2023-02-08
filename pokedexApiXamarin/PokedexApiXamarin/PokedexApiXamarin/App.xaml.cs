c48d5affc1a3d7bfc343222a8b3ed0324ca874d5
AlejandroSanchezTapia <neil.tapia11@gmail.com>
2023-02-08 14:30:43 -0600

    Add folders Models & Views

---
+++ pokedexApiXamarin/PokedexApiXamarin/PokedexApiXamarin/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using PokedexApiXamarin.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PokedexApiXamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage( new PokemonList());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ pokedexApiXamarin/PokedexApiXamarin/PokedexApiXamarin/Models/Pokemon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokedexApiXamarin.Models
+{
+    internal class Pokemon
+    {
+
+    }
+}
